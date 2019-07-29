fc022f75da936001f4c1b675978e830fd0231ee7
Emily Y <emilygbs@yahoo.com>
2019-07-29 16:17:59 -0400

    Added file for reading training data

---
+++ read_data.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+
+def read_train():
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    xtrain = []
+    ytrain = []
+
+    path = "asl_alphabet_train/"
+    for letter in range(26):
+        l = uppercase[letter]
+        ytrain.append(l)
+        xtrain.append([])
+        for number in range(1, 3001):
+            xtrain[letter].append(cv2.imread(path + l + "/" + l + str(number) + ".jpg", cv2.IMREAD_COLOR))
+
+    path = "asl_alphabet_train/del/"
+    xtrain.append([])
+    ytrain.append("del")
+    for number in range(1, 3001):
+        xtrain[26].append(cv2.imread(path + "del" + str(number) + ".jpg", cv2.IMREAD_COLOR))
+
+    path = "asl_alphabet_train/nothing/"
+    xtrain.append([])
+    ytrain.append("nothing")
+    for number in range(1, 3001):
+        xtrain[27].append(cv2.imread(path + "nothing" + str(number) + ".jpg", cv2.IMREAD_COLOR))
+
+    path = "asl_alphabet_train/space/"
+    xtrain.append([])
+    ytrain.append("space")
+    for number in range(1, 3001):
+        xtrain[28].append(cv2.imread(path + "space" + str(number) + ".jpg", cv2.IMREAD_COLOR))
+
+    xtrain = np.array(xtrain)
+    ytrain = np.array(ytrain)
+
+    return xtrain, ytrain
