142f5c9e7f64dca2f6688d1b3d480d6c633cd011
Justin Kamal <kamaljustin@yahoo.com>
2019-07-30 17:02:02 -0400

    added fortune cookie stuff

---
+++ fortune_cookie_fortune_teller/generator.py
@@ -0,0 +1,19 @@
+import fortune_tell
+
+
+path_to_fortunes = "./proverb/fortune_bank.txt"
+with open(path_to_fortunes, "r", encoding="utf8") as f:
+    fortunes = f.read()
+print(str(len(fortunes)) + " character(s)")
+chars = set(fortunes)
+print("'~' is a good pad character: ", "~" not in chars)
+
+fortunes = fortunes.split("\n")
+
+
+lm3 = fortune_tell.train_lm(fortunes, 3)
+
+def tell_fortune():
+    return fortune_tell.generate_text(lm3, 3)
+
+
